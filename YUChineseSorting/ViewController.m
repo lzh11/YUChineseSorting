989ad9fccedf292e8714777826f1f77724fc2179
BruceYu <c6357@outlook.com>
2016-06-28 12:46:28 +0800

    优化界面

--- YUChineseSorting/ViewController.m
+++ YUChineseSorting/ViewController.m
@@ -9,9 +9,15 @@
 #import "ViewController.h"
 #import "ChineseString.h"
 
+#define APP_WIDTH [[UIScreen mainScreen]applicationFrame].size.width
+#define APP_HEIGHT [[UIScreen mainScreen]applicationFrame].size.height
+
 @interface ViewController ()
 @property(nonatomic,strong)NSMutableArray *indexArray;
 @property(nonatomic,strong)NSMutableArray *letterResultArr;
+
+@property (nonatomic, strong) UILabel *sectionTitleView;
+@property (nonatomic, strong) NSTimer *timer;
 @end
 
 @implementation ViewController
@@ -21,17 +27,29 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    NSArray *stringsToSort=[NSArray arrayWithObjects:
-                            @"￥hhh, .$",@" ￥Chin ese ",@"开源中国 ",@"www.oschina.net",
-                            @"开源技术",@"社区",@"开发者",@"传播",
-                            @"2014",@"a1",@"100",@"中国",@"暑假作业",
-                            @"键盘", @"鼠标",@"hello",@"world",@"b1",
-                            nil];
+    self.sectionTitleView = ({
+            UILabel *sectionTitleView = [[UILabel alloc] initWithFrame:CGRectMake((APP_WIDTH-100)/2, (APP_HEIGHT-100)/2,100,100)];
+            sectionTitleView.textAlignment = NSTextAlignmentCenter;
+            sectionTitleView.font = [UIFont boldSystemFontOfSize:60];
+            sectionTitleView.textColor = [UIColor blueColor];
+            sectionTitleView.backgroundColor = [UIColor whiteColor];
+            sectionTitleView.layer.cornerRadius = 6;
+            sectionTitleView.layer.borderWidth = 1.f/[UIScreen mainScreen].scale;
+            _sectionTitleView.layer.borderColor = [UIColor groupTableViewBackgroundColor].CGColor;
+        sectionTitleView;
+    });
+    [self.navigationController.view addSubview:self.sectionTitleView];
+    self.sectionTitleView.hidden = YES;
     
+    
+    NSArray *stringsToSort = [NSArray arrayWithObjects:
+                              @"￥hhh, .$",@" ￥Chin ese ",@"开源中国 ",@"www.oschina.net",
+                              @"开源技术",@"社区",@"开发者",@"传播",
+                              @"2014",@"a1",@"100",@"中国",@"暑假作业",
+                              @"键盘", @"鼠标",@"hello",@"world",@"b1",
+                              nil];
     self.indexArray = [ChineseString IndexArray:stringsToSort];
     self.letterResultArr = [ChineseString LetterSortArray:stringsToSort];
-    
-    
 }
 
 
@@ -60,29 +78,48 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil)
-    {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    }
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     cell.textLabel.text = [[self.letterResultArr objectAtIndex:indexPath.section]objectAtIndex:indexPath.row];
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
 {
+    [self showSectionTitle:title];
     return index;
 }
+
+
+#pragma mark - private
+- (void)timerHandler:(NSTimer *)sender
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [UIView animateWithDuration:.3 animations:^{
+            self.sectionTitleView.alpha = 0;
+        } completion:^(BOOL finished) {
+            self.sectionTitleView.hidden = YES;
+        }];
+    });
+}
+
+-(void)showSectionTitle:(NSString*)title{
+    [self.sectionTitleView setText:title];
+    self.sectionTitleView.hidden = NO;
+    self.sectionTitleView.alpha = 1;
+    [self.timer invalidate];
+    self.timer = nil;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerHandler:) userInfo:nil repeats:NO];
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+
 
 #pragma mark - 
 #pragma mark - UITableViewDelegate
 -(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
-    lab.backgroundColor = [UIColor grayColor];
+    UILabel *lab = [UILabel new];
+    lab.backgroundColor = [UIColor groupTableViewBackgroundColor];
     lab.text = [self.indexArray objectAtIndex:section];
     lab.textColor = [UIColor whiteColor];
     return lab;
